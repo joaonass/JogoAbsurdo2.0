1a71732769d5430cc06f8abaf648690a420233be
Vinicius Costa Lopes <viniciuslopes@soulasalle.com.br>
2026-06-17 16:06:09 -0300

    menu-primeira-versao

---
+++ Jogo-Abisurdo-Projeto/My project (14)/Assets/Assets/Scripts/jogar.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class jogar : MonoBehaviour
+{
+   
+    public void Jogar()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+}
